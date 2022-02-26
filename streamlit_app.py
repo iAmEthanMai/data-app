2af6c5cc3aec9ad7022bc1127995c11836415afb
Ethan Mai <iam.ethanmai@gmail.com>
2022-02-26 16:18:52 -0500

    hide streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,12 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-	    .viewerBadge_link__1S137 {visibility: hidden;}
+
+
+	    .viewerBadge_container__1QSob {visiblity: hidden;}
+
+
+
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
